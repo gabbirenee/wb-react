7029612e23f529d4ba0f86c0e6ee50896097b291
Gabbi Forsythe <gforsythe17@gmail.com>
2019-12-31 17:38:16 -0500

    Added first compnent, Store Picker

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from 'react';  // import them dependencies
+
+// every component is it's own class in React 
+class StorePicker extends React.Component {
+    render(){
+        return <p>Store picker</p>
+    }
+}
+
+export default StorePicker; 
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,7 @@
-// let's go!
+// import your dependencies
+import React from 'react';
+//alternative import statement: import { Component } from 'react';
+import { render } from 'react-dom';     //in first import statement, we need the whole package; on this import statement we only want the render method
+import StorePicker from './components/StorePicker';     // when importing components, import using the file name
+
+render(<StorePicker />, document.querySelector("#main")); 
