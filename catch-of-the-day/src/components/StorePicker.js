bcd8e7caae55fa8e6e87c508624aec568a26ad69
Gabbi Forsythe <gforsythe17@gmail.com>
2020-01-04 22:58:05 -0500

    Used refs to give functionality to store picker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,13 +1,39 @@
 import React from 'react';  // import them dependencies
+import { getFunName } from "../helpers"; 
 
 // every component is it's own class in React 
 class StorePicker extends React.Component {
+    // constructor() {
+    //     super();        // runs the component that is being extended first
+    //     this.goToStore = this.goToStore.bind(this);     // binds StorePicker to this
+    // }
+
+    myInput = React.createRef();
+    goToStore = (event) => {
+        event.preventDefault();     // stops the form from submitting
+        // get the text from the input
+            // can't use keyword "this" here because of how it binds; "this" is undefined here
+            // can be fixed using the constructor function
+            // can also be fixed by writing it as a property with an arrow function 
+        console.log(this);
+        const storeName = this.myInput.current.value;
+        console.log(storeName);      // react be crazy am I right or am I right
+        // change the page to /store/user-input (no refresh or anything like that)
+            // done using react router - StorePicker component is a child of React Router 
+        this.props.history.push(`/store/${storeName}`);
+    };
+
     render(){
         return ( 
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 { /* we in javascript now homies */ }
                 <h2>Please Enter A Store</h2>
-                <input type = "text" placeholder="Store Name" required />
+                <input 
+                    type = "text" 
+                    placeholder="Store Name" 
+                    ref = {this.myInput}
+                    required 
+                    defaultValue={getFunName()} /> 
                 <button type = "submit">Visit Store</button>
             </form>
         );
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 //alternative import statement: import { Component } from 'react';
 import { render } from 'react-dom';     //in first import statement, we need the whole package; on this import statement we only want the render method
 // when importing components, import using the file name
-import App from "./components/App";  
+// import App from "./components/App";  // not used; replaced with router in the render function
 import Router from "./components/Router";
 import './css/style.css';   // importing the css stuff
 
