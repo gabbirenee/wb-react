b7f00f2eefae2b59dbf9c5ba3cb228e4eebc5970
Gabbi Forsythe <gforsythe17@gmail.com>
2020-01-05 22:14:41 -0500

    added the addfishform and added it to the App components state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,24 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    };
+
+    addFish = fish => {
+        // To Update the State:
+            // 1. Take a Copy of the existing state -- never want to update state directly
+        const fishes = {...this.state.fishes};  // takes a copy
+            // 2. add new fish to the copy 
+        fishes[`fish${Date.now()}`] = fish; 
+            // 3. set new object to state
+        this.setState({
+            fishes: fishes  // overwrites the existing state -- will activate a change in react and update accordingly
+            // fishes   // can put this for simplicity sake since they are the same name
+        });
+    }
+
     render() {
         return (
             <div className = "catch-of-the-day">
@@ -11,7 +29,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market" />
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} />
             </div>
         );
     }
